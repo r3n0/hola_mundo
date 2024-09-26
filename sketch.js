bbcbf6e5e31563c3b0257c145a6d3cc599ec4ed2
René Martínez <hola@r3n0.com>
2024-09-26 15:55:52 -0500

    se hicieron mejoras

--- sketch.js
+++ sketch.js
@@ -6,9 +6,15 @@ function setup() {
 }
 
 function draw() {
-	background(10, random(0, 100), 255);
-	fill(mouseY, 255, 0);
+	background(10, random(0, 100), 255, 10);
+
+	strokeWeight(5);
+	noFill();
+	stroke(mouseY, 255, 0);
 	circle(mouseX + 80, mouseY, mouseX);
+
+	strokeWeight(1);
+	stroke(0, 255, 0);
 	fill(200, 60, 0);
 	rect(mouseX, mouseY, 100, 100);
 }
